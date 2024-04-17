077f8045c19f3a0002089cbcda93a45acce3602a
Emre Simsek <emresimseka@gmail.com>
2024-04-17 11:18:50 +0300

    feat/Homepage components
    1. Lazy loading and placeholder
    2. HomePage blog component
    3.HomePage bottom component
    4.Footer Component
    4.Footer Slider (vue3-carousel-nuxt)

--- components/NavBarDelivery.vue
+++ components/NavBarDelivery.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex items-center mr-4 ml-4">
-    <img
-      src="../assets/images/car.webp"
-      alt="delivery_icon"
+    <NuxtImg
+      src="/car.webp"
+      format="webp"
       width="40px"
       height="20px"
       class="w-10 h-auto"
-    />
+    >
+    </NuxtImg>
+
     <div class="ml-4 leading-none whitespace-no-wrap flex-grow">
       <div class="font-bold text-gray-600 uppercase">Free Delivery</div>
       <div class="class=" text-sm italic>on orders over £60*</div>
